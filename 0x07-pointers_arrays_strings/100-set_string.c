d5c8ee278015d3e1c9669e68548fc54c0758c48d
Alemweldaykidane <Alemw9@gmail.com>
2022-04-25 07:55:46 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints chessboard.
- *
- * @a: rows.
- * Return: nothing.
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
  */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+void set_string(char **s, char *to)
+{
+	*s = to;
 }
